f542fc25034e8f80b406d0324eb7c1d914182c20
mazenmu44-max <mazenmu44@gmail.com>
2026-08-20 22:41:42 +0300

    Delete Events directory

--- Events/ready.js
+++
@@ -1,8 +0,0 @@
-const client = require('../bleed')
-client.on('ready', async () => {
-    client.user.setActivity('discord.gg/shdws', { type : 'COMPETING' })
-setInterval(() => {
-client.user.setActivity('discord.gg/shdws', { type : 'COMPETING' })
-}, 600000)
-})
-
